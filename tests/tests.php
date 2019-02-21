e6d542386c916dbba81b0bb4fe5b3e3751c2a47a
Nathan Connor <nxc5929@rit.edu>
2019-02-20 20:00:31 -0500

    Added testng framework

--- tests/tests.php
+++ tests/tests.php
@@ -2,31 +2,33 @@
 
 require FileSystem.php
 
-class Tests extends PHPUnit_Framework_TestCase{
-	
+use PHPUnit\Framework\TestCase;
+
+class Tests extends TestCase{
+	use PHPUnit\Framework\TestCase;
 	public $fileSystemInstance;
 	
-	public function setup(){
+	public function setup(): void{
 		$this->fileSystemInstance = new FileSystem();
 	}
 	
-	public function testCreate(){
+	public function testCreate(): void{
 		$this->assertEquals(1, 1);
 	}
 	
-	public function testDelete(){
+	public function testDelete(): void{
 		$this->assertEquals(1, 1);
 	}
 	
-	public function testView(){
+	public function testView(): void{
 		$this->assertEquals(1, 1);
 	}
 	
-	public function testNetcat(){
+	public function testNetcat(): void{
 		$this->assertEquals(1, 1);
 	}
 	
-	public function testTraversal(){
+	public function testTraversal(): void{
 		$this->assertEquals(1, 1);
 	}
 }
