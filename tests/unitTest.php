6a4fca89cfbbdf6d33dc92d07c593ad6422ed28b
Nathan Connor <nxc5929@rit.edu>
2019-02-20 20:08:50 -0500

    Removed use

--- tests/unitTest.php
+++ tests/unitTest.php
@@ -2,33 +2,31 @@
 
 require FileSystem.php
 
-use PHPUnit\Framework\TestCase;
-
 class Tests extends TestCase{
 	use PHPUnit\Framework\TestCase;
 	public $fileSystemInstance;
 	
-	public function setup(): void{
+	public function setup(){
 		$this->fileSystemInstance = new FileSystem();
 	}
 	
-	public function testCreate(): void{
+	public function testCreate(){
 		$this->assertEquals(1, 1);
 	}
 	
-	public function testDelete(): void{
+	public function testDelete(){
 		$this->assertEquals(1, 1);
 	}
 	
-	public function testView(): void{
+	public function testView(){
 		$this->assertEquals(1, 1);
 	}
 	
-	public function testNetcat(): void{
+	public function testNetcat(){
 		$this->assertEquals(1, 1);
 	}
 	
-	public function testTraversal(): void{
+	public function testTraversal(){
 		$this->assertEquals(1, 1);
 	}
 }
