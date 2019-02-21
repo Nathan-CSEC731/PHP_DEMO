3febc1bcec4b1ba2d28fa4128e0fecf586c67c9d
Nathan Connor <nxc5929@rit.edu>
2019-02-20 19:40:27 -0500

    Initial Commit

---
+++ FileSystem.php
@@ -0,0 +1,24 @@
+<?php
+
+class FileSystem{
+	
+	public function createFile($name, $contents){
+		$myfile = fopen($name, "w");
+		fwrite($myfile, $contents);
+		fclose($myfile);
+	}
+	
+	public function deleteFile($name){
+		unlink($name)
+	}
+	
+	public function viewFile($name){
+		$myfile = fopen($name, "r");
+		$content = fread($myfile, filesize($name));
+		fclose($myfile);
+		return $content;
+	}
+	
+}
+
+?>
